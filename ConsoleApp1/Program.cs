f839b3078666fbd9906528b2535892f49986f888
Duem07 <Familia Ortega@LAPTOP-QOR5U70Q>
2022-11-07 18:37:20 -0600

    Agregar archivos de proyecto.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int año;
+            Console.WriteLine("Ingrese un año: ");
+            año = int.Parse(Console.ReadLine()); //Le pide al usuario que ingrese el año
+            {
+                if (año % 4 == 0 && año % 100 != 0)
+                {
+                    Console.WriteLine("El año {0} si es bisiesto", año);
+                } //Es una condicion para que diga que el año debe ser divisor entre 4, entre 100 y entre 400
+
+
+                else if (año % 400 == 0)
+                {
+
+                    Console.WriteLine("El año {0} si es bisiesto", año);
+                }
+                else
+                {
+                    Console.WriteLine("El año {0} no es bisiesto ", año);
+                } //dice que si no es ninguna de las demas que muestre que no es bisiesto 
+                Console.ReadKey();
+
+            }
+        }
+    
+    }
+}
